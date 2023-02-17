ae77723f6af7aa4cad7752c411db1f53b353755f
William-Hiatt <william.hiatt@wsu.edu>
2023-02-16 20:00:10 -0800

    Added a background and confined user within the white playing area.

--- 487Project/487Project/Game1.cs
+++ 487Project/487Project/Game1.cs
@@ -1,12 +1,14 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System.ComponentModel;
 
 namespace MainProgram
 {
     public class Game1 : Game
     {
         UserSprite user;
+        Texture2D background;
 
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
@@ -14,6 +16,8 @@ namespace MainProgram
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
+            _graphics.PreferredBackBufferWidth = 1600;
+            _graphics.PreferredBackBufferHeight = 1400;
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
         }
@@ -33,6 +37,7 @@ namespace MainProgram
             // TODO: use this.Content to load your game content here
             // user.ballTexture = Content.Load<Texture2D>("ball");
             user = new UserSprite(Content.Load<Texture2D>("ball"), _graphics);
+            background = Content.Load<Texture2D>("tempoverlay");
         }
 
         protected override void Update(GameTime gameTime)
@@ -55,6 +60,7 @@ namespace MainProgram
 
             // TODO: Add your drawing code here
             _spriteBatch.Begin();
+            _spriteBatch.Draw(background, new Rectangle(0, 0, 1600, 1400), Color.White);
             user.Draw(_spriteBatch);
             _spriteBatch.End();
 
--- 487Project/487Project/UserSprite.cs
+++ 487Project/487Project/UserSprite.cs
@@ -18,7 +18,7 @@ namespace MainProgram
         public UserSprite(Texture2D ballTexture, GraphicsDeviceManager _graphics)
         {
             this.ballTexture = ballTexture;
-            this.ballPosition = new Vector2(_graphics.PreferredBackBufferWidth / 2, _graphics.PreferredBackBufferHeight / 2);
+            this.ballPosition = new Vector2(500, 1200);
             this.ballSpeed = 100f;
         }
 
@@ -43,49 +43,51 @@ namespace MainProgram
 
             if (kstate.IsKeyDown(Keys.Up))
             {
-                tempV.Y -= this.ballSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+                tempV.Y -= this.ballSpeed * 6 * (float)gameTime.ElapsedGameTime.TotalSeconds;
                 this.ballPosition = tempV;
             }
 
             if (kstate.IsKeyDown(Keys.Down))
             {
-                tempV.Y += this.ballSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+                tempV.Y += this.ballSpeed * 6 * (float)gameTime.ElapsedGameTime.TotalSeconds;
                 this.ballPosition = tempV;
             }
 
             if (kstate.IsKeyDown(Keys.Left))
             {
-                tempV.X -= this.ballSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+                tempV.X -= this.ballSpeed * 6 * (float)gameTime.ElapsedGameTime.TotalSeconds;
                 this.ballPosition = tempV;
             }
 
             if (kstate.IsKeyDown(Keys.Right))
             {
-                tempV.X += this.ballSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+                tempV.X += this.ballSpeed * 6 * (float)gameTime.ElapsedGameTime.TotalSeconds;
                 this.ballPosition = tempV;
             }
 
-            if (this.ballPosition.X > _graphics.PreferredBackBufferWidth - this.ballTexture.Width / 2)
+            if (this.ballPosition.X > 1013 - this.ballTexture.Width / 2)
             {
-                tempV.X = _graphics.PreferredBackBufferWidth - this.ballTexture.Width / 2;
+                tempV.X = 1013 - this.ballTexture.Width / 2;
                 this.ballPosition = tempV;
             }
-            else if (this.ballPosition.X < this.ballTexture.Width / 2)
+            else if (this.ballPosition.X < 89 + this.ballTexture.Width / 2)
             {
-                tempV.X = this.ballTexture.Width / 2;
+                tempV.X = 89 + this.ballTexture.Width / 2;
                 this.ballPosition = tempV;
             }
 
-            if (this.ballPosition.Y > _graphics.PreferredBackBufferHeight - this.ballTexture.Height / 2)
+
+            if (this.ballPosition.Y > 1266 - this.ballTexture.Height / 2)
             {
-                tempV.Y = _graphics.PreferredBackBufferHeight - this.ballTexture.Height / 2;
+                tempV.Y = 1266 - this.ballTexture.Height / 2;
                 this.ballPosition = tempV;
             }
-            else if (this.ballPosition.Y < this.ballTexture.Height / 2)
+            else if (this.ballPosition.Y < 104 + this.ballTexture.Height / 2)
             {
-                tempV.Y = this.ballTexture.Height / 2;
+                tempV.Y = 104 + this.ballTexture.Height / 2;
                 this.ballPosition = tempV;
             }
+            
         }
 
     }
