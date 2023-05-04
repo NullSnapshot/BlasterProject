39edfb896e661000838d33228e7a00610fc187f9
Null <184932-NullSnapshot@users.noreply.gitlab.com>
2023-05-04 12:03:30 -0700

    Adds follow player behavior

--- 487Project/487Project/Game/Entities/Behaviors/Mob/EnemyBehavior.cs
+++ 487Project/487Project/Game/Entities/Behaviors/Mob/EnemyBehavior.cs
@@ -15,7 +15,7 @@ namespace BulletBlaster.Game.Entities.Behaviors.Mob
         public EnemyBehavior(EnemyConfig behavior)
         {
             this.TargetPosition = new Vector2(behavior.position.x, behavior.position.y);
-            float speed = behavior.enemyMovement.movement_speed;
+            this.TargetSpeed = behavior.enemyMovement.movement_speed;
         }
 
         public override abstract void Update(GameTime gameTime);
---
+++ 487Project/487Project/Game/Entities/Behaviors/Mob/FollowPlayerBehavior.cs
@@ -0,0 +1,34 @@
+﻿using BulletBlaster.Game.config;
+using BulletBlaster.Game.Controllers;
+using Microsoft.Xna.Framework;
+using MonoGame.Extended;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulletBlaster.Game.Entities.Behaviors.Mob
+{
+    internal class FollowPlayerBehavior : EnemyBehavior
+    {
+        private int targetY = 0;
+
+        public static string Name => "follow-player";
+        public FollowPlayerBehavior() { }
+        public FollowPlayerBehavior(EnemyConfig config) : base(config) 
+        {
+            targetY = config.position.y;
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            Vector2 playerPos = EntityManager.GetPlayerPos();
+            playerPos.Y = targetY;
+            Vector2 currentPos = this.TargetPosition;
+
+            Vector2 wantedPosition = (playerPos - currentPos).NormalizedCopy() * new Vector2((float)Math.Sqrt(this.TargetSpeed),0);
+            this.TargetPosition += wantedPosition;
+        }
+    }
+}
